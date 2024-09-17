789dcff916b4e6eecd9ead31b82c3937f21b242f
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-09-17 16:56:57 +0300

    view binding and status bar color added

--- app/src/main/java/com/android/example/quizappuimvvm/MainActivity.kt
+++ app/src/main/java/com/android/example/quizappuimvvm/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.android.example.quizappuimvvm
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.example.quizappuimvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.grey)
+
     }
 }
